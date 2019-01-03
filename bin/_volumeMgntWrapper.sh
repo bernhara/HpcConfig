2fa7073a63888f4bcf8c09fb371e0aacca40deaf
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-01-03 16:01:32 +0100

    ok if volumes exists

--- bin/_volumeMgntWrapper.sh
+++ bin/_volumeMgntWrapper.sh
@@ -3,6 +3,12 @@
 COMMAND_PATH="$0"
 COMMAND=$( basename "${COMMAND_PATH}" )
 
+DOCKER_ENPOINT_LIST="
+   s-raph-ale:2375
+   s-huaweig560:2375
+   s-cocolink:2375
+"
+
 user="$1"
 
 if [ -z "${user}" ]
@@ -16,7 +22,7 @@ NotImplemented ()
     echo "${COMMAND} not implemented" 1>&2
 }
 
-CreateDockerVolumeFromGlusterVolume ()
+CreateDockerVolumesFromGlusterVolume ()
 {
 
     user_name="$1"
@@ -24,19 +30,35 @@ CreateDockerVolumeFromGlusterVolume ()
     gluster_volume_name="gv_${user_name}"
     docker_volume_name="gv_${user_name}"
 
-    docker volume create \
-	--driver local \
-	--opt type=nfs \
-	--opt o=addr=localhost,rw,noatime,rsize=8192,wsize=8192,tcp,timeo=14 \
-	--opt device=:/${gluster_volume_name} \
-	${docker_volume_name}
+    for endpoint in ${DOCKER_ENPOINT_LIST}
+    do
+
+	volumes=$(
+	    docker -H "${endpoint}" \
+		volume ls -q --filter "Name=${docker_volume_name}"
+	)
+
+	if [ -n "${volumes}" ]
+	then
+	    echo "Volume ${docker_volume_name} already exists on endpoint ${endpoint}. Skipping".
+	else
+
+	    docker -H "${endpoint}" \
+		volume create \
+		--driver local \
+		--opt type=nfs \
+		--opt o=addr=localhost,rw,noatime,rsize=8192,wsize=8192,tcp,timeo=14 \
+		--opt device=:/${gluster_volume_name} \
+		${docker_volume_name}
+	fi
+    done
 }
 
 
 case "${COMMAND}" in
 
-    "createDockerVolume4User.sh" )
-	CreateDockerVolumeFromGlusterVolume "${user}"
+    "createDockerVolumes4User.sh" )
+	CreateDockerVolumesFromGlusterVolume "${user}"
 	;;
 
     "createGlusterVolume4User.sh" )
